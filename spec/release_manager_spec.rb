5bdac35062f3f330b24740cae8cdd5e570bd788b
Faizal Zakaria <fai@code3.io>
2017-12-31 13:41:16 +0200

    Init commit

---
+++ spec/release_manager_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe ReleaseManager do
+  it "has a version number" do
+    expect(ReleaseManager::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
